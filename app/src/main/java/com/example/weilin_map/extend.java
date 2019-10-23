2a97fdb2200c03327821267b62b2dea4b643de05
Boris <907672942@qq.com>
2019-10-23 14:13:34 +0800

    地图的练习

---
+++ app/src/main/java/com/example/weilin_map/extend.java
@@ -0,0 +1,13 @@
+package com.example.weilin_map;
+
+import java.util.List;
+
+public class extend {
+    private List<mapint_info>request_01;
+
+
+    @Override
+    public String toString() {
+        return "request:{"+request_01+"}";
+    }
+}
---
+++ app/src/main/java/com/example/weilin_map/modeSensor.java
@@ -0,0 +1,11 @@
+package com.example.weilin_map;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class modeSensor {
+
+
+}
